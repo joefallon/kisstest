e95bbbe125bda33838a5a50dff0651b0cb9afdd1
Joe Fallon <joseph.t.fallon@gmail.com>
2015-04-01 08:17:55 -0700

    Work was started to integrate code coverage.

--- tests/index.php
+++ tests/index.php
@@ -4,9 +4,13 @@ use JoeFallon\KissTest\UnitTest;
 /**
  * @author    Joseph Fallon <joseph.t.fallon@gmail.com>
  * @copyright Copyright 2014 Joseph Fallon (All rights reserved)
+ *
  * @license   MIT
  */
 require('config/main.php');
+
+$coverage = new PHP_CodeCoverage;
+$coverage->start('All tests');
 
 new tests\JoeFallon\KissTest\AbstractFactoryTests();
 new tests\JoeFallon\KissTest\MockTests();
@@ -14,5 +18,11 @@ new tests\JoeFallon\KissTest\Reporting\MilliTimespanTests();
 new tests\JoeFallon\KissTest\Reporting\SummaryTests();
 new tests\JoeFallon\KissTest\Reporting\TestCaseResultTests();
 new tests\JoeFallon\KissTest\Reporting\UnitTestResultTests();
+
+
+$coverage->stop();
+
+$writer = new PHP_CodeCoverage_Report_HTML;
+$writer->process($coverage, '../cov');
 
 UnitTest::getAllUnitTestsSummary();

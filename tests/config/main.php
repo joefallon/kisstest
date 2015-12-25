144e91ca49dd36bd99c98198e08faf615bb1fdfb
Joe Fallon <joseph.t.fallon@gmail.com>
2015-12-25 13:37:50 -0800

    Code cleanup was performed.

--- tests/config/main.php
+++ tests/config/main.php
@@ -1,12 +1,6 @@
 <?php
-/**
- * @author    Joseph Fallon <joseph.t.fallon@gmail.com>
- *
- * @copyright Copyright 2014 Joseph Fallon (All rights reserved)
- *
- * @license   MIT
- */
 use JoeFallon\AutoLoader;
+use JoeFallon\KissTest\UnitTest;
 
 // Define the include paths.
 define('BASE_PATH', realpath(dirname(__FILE__).'/../../'));
@@ -16,6 +10,12 @@ define('VEND_PATH', BASE_PATH.'/vendor');
 // Set the application include paths for autoloading.
 set_include_path(get_include_path().':'.SRC_PATH.':'.BASE_PATH);
 
+
 require(VEND_PATH.'/autoload.php');
 
 AutoLoader::registerAutoLoad();
+
+//UnitTest::setCodeCoverageOutputDirectory('../cov');
+//UnitTest::addDirectoryToCoverageBlacklist('../tests');
+//UnitTest::addDirectoryToCoverageBlacklist('../vendor');
+//UnitTest::setCodeCoverageEnabled(false);
--- tests/index.php
+++ tests/index.php
@@ -1,18 +1,6 @@
 <?php
 use JoeFallon\KissTest\UnitTest;
-
-/**
- * @author    Joseph Fallon <joseph.t.fallon@gmail.com>
- * @copyright Copyright 2014 Joseph Fallon (All rights reserved)
- *
- * @license   MIT
- */
 require('config/main.php');
-
-UnitTest::setCodeCoverageOutputDirectory('../cov');
-//UnitTest::addDirectoryToCoverageBlacklist('../tests');
-UnitTest::addDirectoryToCoverageBlacklist('../vendor');
-UnitTest::setCodeCoverageEnabled(true);
 
 new tests\JoeFallon\KissTest\AbstractFactoryTests();
 new tests\JoeFallon\KissTest\MockTests();
@@ -21,6 +9,5 @@ new tests\JoeFallon\KissTest\Reporting\MilliTimespanTests();
 new tests\JoeFallon\KissTest\Reporting\SummaryTests();
 new tests\JoeFallon\KissTest\Reporting\TestCaseResultTests();
 new tests\JoeFallon\KissTest\Reporting\UnitTestResultTests();
-
 
 UnitTest::getAllUnitTestsSummary();
